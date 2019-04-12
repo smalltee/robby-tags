c44b963428a4147340e4c9da8f566808a66cbaf6
Robby <Robby.Lian@paytmmall.com>
2019-04-12 18:41:44 +0800

    first commit for this uni control

---
+++ components/robby-tags/robby-tags.vue
@@ -0,0 +1,112 @@
+<template>
+	<view class="tagController">
+		<view class="tagContainer">
+			<view class="tagItem" :class="tagBgColor" v-bind:key="index" v-for="(tagText,index) in tagList">
+				<text @tap="tapTag" :data-text="tagText">{{tagText}}</text>
+				<text v-if="isShowDelIcon" class="tagDelIcon" @tap="delTag" :data-text="tagText">x</text>
+			</view>
+		</view>
+		<view class="tagInput">
+			<input type="text" v-model="tagString" placeholder="输入新的标签,逗号间隔" />
+			<button type="default" size="mini" @tap="createTags">添加</button>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		name:'robby-tags',
+		props: ['enableDel','bgColorType','tagData'],
+		data() {
+			return {
+				tagString:'',
+				tagList: this.tagData || [],
+				isShowDelIcon: this.enableDel || false
+			}
+		},
+		computed:{
+			tagBgColor: function(){
+				if(this.bgColorType === null){
+					return 'tagBgDefault'
+				}else if(this.bgColorType === 'primary'){
+					return 'tagBgPrimary'
+				}else if(this.bgColorType === 'success'){
+					return 'tagBgSuccess'
+				}else if(this.bgColorType === 'warn'){
+					return 'tagBgWarning'
+				}else if(this.bgColorType === 'error'){
+					return 'tagBgError'
+				}else{
+					return 'tagBgDefault'
+				}
+			}
+		},
+		methods: {
+			createTags: function(){
+				if(this.tagString.length>0){
+					let newTagList = this.tagString.split(/,|，/)
+					for(let i=0;i<newTagList.length;i++){
+						let newTag = newTagList[i].trim()
+						
+						if(newTag !== '' && this.tagList.indexOf(newTag)<=0){
+							this.tagList.push(newTag)
+						}
+					}
+				}
+				this.tagString = ''
+			}, 
+			delTag: function(e){
+				let delTagText = e.currentTarget.dataset.text
+				let delTagIndex = this.tagList.indexOf(delTagText)
+				this.tagList.splice(delTagIndex,1)
+				this.$emit("delete", delTagText)
+			},
+			tapTag: function(e){
+				let selTagText = e.currentTarget.dataset.text
+				this.$emit("click", selTagText)
+			}
+		}
+	}
+</script>
+
+<style>
+	.tagController{
+		padding: 10upx;	
+	}
+	.tagContainer{
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: flex-start;
+	}
+	.tagItem{
+		padding: 10upx 20upx;
+		margin: 10upx;
+		border-radius: 40upx;
+		color: white;
+	}
+	.tagBgDefault{
+		background-color: #b0b0b0;
+	}
+	.tagBgPrimary{
+		background-color: #007aff;
+	}
+	.tagBgSuccess{
+		background-color: #4cd964;
+	}
+	.tagBgWarning{
+		background-color: #f0ad4e;
+	}
+	.tagBgError{
+		background-color: #dd524d;
+	}
+	.tagDelIcon{
+		padding-left: 20upx;
+	}
+	.tagInput{
+		padding: 10upx;
+	}
+	.tagInput input{
+		width: 60%;
+		display: inline-block;
+	}
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,32 @@
+<template>
+	<view>
+		<robby-tags :enable-del="showDel"></robby-tags>
+	</view>
+</template>
+
+<script>
+	import robbyTags from '@/components/Robby-tags/Robby-tags.vue'
+	
+	export default {
+		components:{robbyTags},
+		data() {
+			return {
+				// tabData:['物理','数学'],
+				showDel: true,
+// 				colorType: 'error'
+			}
+		},
+		methods:{
+			clickTag: function(e){
+				console.log(e)
+			},
+			deleteTag: function(e){
+				console.log(e)
+			}
+		}
+	}
+</script>
+
+<style>
+	
+</style>
